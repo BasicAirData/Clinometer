a4eeb579b07cadee420dbb1158ac10f0d8144f8f
Graziano Capelli <air@shweb.it>
2021-01-28 17:02:06 +0100

    Keep the navigation Bar visible

--- app/src/main/java/eu/basicairdata/clinometer/ClinometerActivity.java
+++ app/src/main/java/eu/basicairdata/clinometer/ClinometerActivity.java
@@ -318,11 +318,11 @@ public class ClinometerActivity extends AppCompatActivity implements SensorEvent
                 // Set the content to appear under the system bars so that the
                 // content doesn't resize when the system bars hide and show.
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN //|
+                //View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
                 // Hide the nav bar and status bar
                 //View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                //View.SYSTEM_UI_FLAG_FULLSCREEN
+                View.SYSTEM_UI_FLAG_FULLSCREEN
         );
 
         this.getWindow().setFlags(
